98f200145771b7da8672dcab0bc875301bd743e1
richiecakes <richard.appleby@wadh.ox.ac.uk>
2016-04-27 23:48:34 +0100

    Documentation for type contexts.

--- src/Printype/Principal/Contexts.hs
+++ src/Printype/Principal/Contexts.hs
@@ -8,14 +8,21 @@ A type context can be implemented as a map from term variables to types.
 -}
 
 module Printype.Principal.Contexts(
+  -- $contexts
+
+  -- * Datatype
   Context,
+  -- * Construction
   empty,
   singleton,
-  toList,
+  -- * Update
+  union,
+  remove,
+  -- * Query
   lookup,
   commonSubjects,
-  remove,
-  union) where
+  -- * Conversion
+  toList) where
 
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
@@ -27,9 +34,13 @@ import Printype.Lambda
 import Printype.Types
 import Printype.Types.Substitutions hiding (empty, singleton, remove)
 
--- TYPE CONTEXTS --
+-- $contexts
+-- Standard implementation of type contexts (environments) using Map.
+-- This module is intended to be imported qualified, e.g.
+--
+-- > import qualified Printype.Principal.Contexts as Ctx
 
--- A context under which a particular term is typed.
+-- |A context under which a particular term is typed.
 -- eg. {x:A, y:A => B}
 newtype Context = Ctx (Map.Map Char Type)
 
@@ -43,31 +54,33 @@ instance Substitutable Context where
   apply c = cmap (apply c)
   subjects (Ctx m) = Set.unions $ Map.elems $ fmap subjects m
 
-cmap :: (Type -> Type) -> Context -> Context
-cmap f (Ctx m) = Ctx $ Map.map f m
-
--- The empty type context {}.
+-- |The empty type context {}.
 empty :: Context
 empty = Ctx $ Map.empty
 
+-- |A singleton type context of the form {x:A}.
 singleton :: Char -> Type -> Context
 singleton x t = Ctx $ Map.singleton x t
 
+-- |View the context as a list of pairs.
 toList :: Context -> [(Char, Type)]
 toList (Ctx m) = Map.toAscList m
 
--- Determine the type given to a particular variable under the type context.
+-- |Determine the type given to a particular variable in the type context.
 lookup :: Context -> Char -> Maybe Type
 lookup (Ctx m) x = Map.lookup x m
 
--- Returns a list containing the variables referenced in both contexts.
+-- |Returns a list containing the variables referenced in both contexts.
 commonSubjects :: Context -> Context -> [Char]
 commonSubjects (Ctx m1) (Ctx m2) = Set.toAscList $ Set.intersection (Map.keysSet m1) (Map.keysSet m2)
 
--- Removes variable x and associated binding from a type context.
+-- |Removes variable x and associated binding from a type context.
 remove :: Context -> Char -> Context
 remove (Ctx m) x = Ctx $ Map.delete x m
 
--- Left biased union of two contexts.
+-- |Left biased union of two contexts.
 union :: Context -> Context -> Context
 union (Ctx c1) (Ctx c2) = Ctx $ Map.union c1 c2
+
+cmap :: (Type -> Type) -> Context -> Context
+cmap f (Ctx m) = Ctx $ Map.map f m
